d91f8ecf6cc679d97259ef91fc178c1f42af5b23
danu0227 <danu@gmail.com>
2020-05-08 02:55:58 +0900

    create phoneme2viseme

--- phoneme2viseme.py
+++ phoneme2viseme.py
@@ -1,5 +1,5 @@
-cmu_phonemes= ['AA', 'AE', 'AH', 'AO', 'AW', 'AY',
-               'B', 'CH', 'D', 'DH', 'EH', 'ER', 'EY', 'F',
-               'G', 'HH', 'IH' 'IY', 'JH', 'K', 'L', 'M',
-               'N', 'NG', 'OW', 'OY', 'P', 'R', 'S', 'SH',
-               'T', 'TH', 'UH', 'UW', 'V', 'W', 'Y', 'Z', 'ZH']
+cmu_phonemes = ['AA', 'AE', 'AH', 'AO', 'AW', 'AY',
+                'B', 'CH', 'D', 'DH', 'EH', 'ER', 'EY', 'F',
+                'G', 'HH', 'IH' 'IY', 'JH', 'K', 'L', 'M',
+                'N', 'NG', 'OW', 'OY', 'P', 'R', 'S', 'SH',
+                'T', 'TH', 'UH', 'UW', 'V', 'W', 'Y', 'Z', 'ZH']
