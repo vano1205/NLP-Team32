98f3ad46734c6c12f43b598ba5d3da9a1e6f69f6
Neila-ros <55741753+Neila-ros@users.noreply.github.com>
2020-06-03 21:57:47 +0900

    runs everything except import_script

---
+++ main.py
@@ -0,0 +1,52 @@
+import sys
+import os
+sys.path.append(os.path.abspath("Import_script"))
+sys.path.append(os.path.abspath("pairkoreng"))
+sys.path.append(os.path.abspath("korean2phoneme"))
+sys.path.append(os.path.abspath("phoneme2viseme"))
+sys.path.append(os.path.abspath("Best_synonyms"))
+sys.path.append(os.path.abspath("sent_similarity"))
+
+from similarity import compare_file
+import best_synonyms
+import pairkoreng
+#from best_synonyms import best_synonyms
+#from pairkoreng import pairkoreng
+
+file_en_1 ="Import_script/Death Bell_ENG.txt"
+file_ko_1 ="Import_script/Death Bell_KOR.txt"
+
+file_en_2 ="Import_script/Bleak Night_ENG.txt"
+file_ko_2 ="Import_script/Bleak Night_KOR.txt"
+
+#pairs = best_synonyms.best_pairs
+
+for i in range(1,3):
+    if i == 1:
+        word_pairs = pairkoreng.pair_words(file_en_1, file_ko_1)
+        pairs = best_synonyms.find_best_syn(file_en_1, file_ko_1, word_pairs)
+        filename = "Results/Death_Bell_translation.txt"
+    else:
+        word_pairs = pairkoreng.pair_words(file_en_2, file_ko_2)
+        pairs = best_synonyms.find_best_syn(file_en_2, file_ko_2, word_pairs)
+        filename = "Results/Bleak_Night_translation.txt"
+
+    f = open(filename,'w')
+
+    for pair in pairs:
+        i=0
+        sent=''
+        while i < len(pair):
+            j=0
+            while j < len(pair[i][1]):
+                if sent != '':
+                    sent = sent+' '+pair[i][1][j]
+                else:#first word, no space before
+                    sent = pair[i][1][j]
+                j=j+1
+            i=i+1
+        f.write(sent+'\n')
+
+    f.close()
+
+    compare_file(filename, file_ko_1)
