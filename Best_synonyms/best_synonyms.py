a518d77d7b0d45dd29a6007657b520141b02f5a0
Neila-ros <55741753+Neila-ros@users.noreply.github.com>
2020-06-13 15:25:12 +0900

    new method

--- Best_synonyms/best_synonyms.py
+++ Best_synonyms/best_synonyms.py
@@ -11,12 +11,12 @@ sys.path.append(os.path.abspath("korean2phoneme"))
 sys.path.append(os.path.abspath("sent_similarity"))
 
 import pairkoreng
-
 from korean2phoneme import kor2phon
 from phoneme2viseme import pho2vi
-
 from similarity import compare_viseme
+from similarity import sents_scores
 import file_visemes
+import scores_Death_Bell
 #import Evaluate_updated
 #from Evaluate_updated import viseme_set_similarity
 
@@ -122,6 +122,43 @@ def best_word(eng_word, eng_vis, ko_vis):
     return (res_word, res_vis)
 
 
+def getScores(word_pair, vis_pair):
+    """returns a sorted list of the scores of each word in a sentence"""
+    scores=[]
+    #get the scores for all word pairs in a sentence
+    i=0
+    while i < len(word_pair):
+        j=0
+        while j < len(word_pair[i][1]):
+            if (len(vis_pair[i][0]) != 0):
+                scores.append((word_pair[i], word_pair[i][1][j], compare_viseme(vis_pair[i][1][j], vis_pair[i][0])))
+            j=j+1
+        i=i+1
+
+    #sort by minimum score
+    sorted_scores = sorted(scores, key=lambda tup: tup[2])
+    return sorted_scores
+
+
+def sents_viseme(visemes):
+    """returns a tuple with the visemes of the korean sentence and the english sentence"""
+    ko_vis=''
+    eng_vis=''
+    for vis in visemes:
+        #korean visemes
+        for v in vis[0]:
+            ko_vis=ko_vis+v
+        i = 0
+        while i < len(vis[1]):
+            l = vis[1][i]
+            for elt in l:
+                for v in elt:
+                    eng_vis=eng_vis+v
+            i=i+1
+    return (ko_vis, eng_vis)
+
+
+
 #def find_best_syn(file_en, file_ko, pairs):
 result_pair = pairkoreng.result_pair
 viseme_list = []
@@ -130,26 +167,71 @@ best_viseme_list = []
 viseme_list = file_visemes.list
 #viseme_list = set_viseme_list(result_pair)
 
-#print("set viseme list finish\n")
-#print(viseme_list)
 best_viseme_list = viseme_list
 best_pairs = result_pair
 
+
+score_list=[]
+sent_score=0
+#sent_score_list=sents_scores("Import_script/Death Bell_ENG.txt", "Import_script/Death Bell_KOR.txt")
+#takes too long so just read file
+sent_score_list=scores_Death_Bell.list
+
+s=0
 b=0
 for word_pair, vis_pair in zip(result_pair, viseme_list):
+    sent_score = sent_score_list[s]
+    s=s+1
     i=0
-    while i < len(word_pair):
-        j=0
-        while j < len(word_pair[i][1]):
-            if (len(vis_pair[i][0]) != 0):
-                best = best_word(word_pair[i][1][j], vis_pair[i][1][j], vis_pair[i][0])
-            #if a better synonym is found:
-            if best[0] != word_pair[i][1][j]:
-                best_pairs[b][i][1][j] = best[0]
-                best_viseme_list[b][i][1][j] = best[1]
-            j=j+1
-        i=i+1
+    p=0
+    #scores for all word pairs in a sentence, sorted by min score
+    pair_scores = getScores(word_pair, vis_pair)
+    #if similarity score of the sentence < threshold
+    while (sent_score < 0.6) and (p < len(pair_scores)):
+        pair=pair_scores[p]
+        exit=0
+        i=0
+        while i < len(word_pair):#sentence length
+            j=0
+            while j < len(word_pair[i][1]):#length of eng part of the pair
+                #find the word to change that has the current lowest score
+                if (word_pair[i][1][j] == pair[1]):
+                    if (len(vis_pair[i][0]) != 0):
+                        best = best_word(word_pair[i][1][j], vis_pair[i][1][j], vis_pair[i][0])
+                        #if a better synonym is found:
+                        if best[0] != word_pair[i][1][j]:
+                            best_pairs[b][i][1][j] = best[0]
+                            best_viseme_list[b][i][1][j] = best[1]
+                        exit=1
+                        break
+                j=j+1
+            if (exit == 1):
+                break
+            i=i+1
+        #evaluate sentence score
+        tuple=sents_viseme(best_viseme_list[b])
+        sent_ko=tuple[0]
+        sent_eng=tuple[1]
+        sent_score = compare_viseme(sent_ko, sent_eng)
+        p=p+1
+
     b=b+1
+
+#b=0
+#for word_pair, vis_pair in zip(result_pair, viseme_list):
+#    i=0
+#    while i < len(word_pair):
+#        j=0
+#        while j < len(word_pair[i][1]):
+#            if (len(vis_pair[i][0]) != 0):
+#                best = best_word(word_pair[i][1][j], vis_pair[i][1][j], vis_pair[i][0])
+#            #if a better synonym is found:
+#            if best[0] != word_pair[i][1][j]:
+#                best_pairs[b][i][1][j] = best[0]
+#                best_viseme_list[b][i][1][j] = best[1]
+#            j=j+1
+#        i=i+1
+#    b=b+1
 
 f1 = open('Best_synonyms/best_pairs.txt','w')
 for pair in best_pairs:
